ac850a230a1999504284cd2d71c92ca88135addc
Matheus Massari <massari@Matheuss-MacBook-Air.local>
2021-12-10 18:20:52 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getStorageTheme() {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
 }
+
+const App = () => {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  function handleTheme() {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    }
+    if (theme === "dark-theme") {
+      setTheme("light-theme");
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    // document.getElementById("root").classList.add(theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Overreacted</h1>
+          <button className="btn" onClick={() => handleTheme()}>
+            toggle
+          </button>
+        </div>
+        <section className="articles">
+          {data.map((item) => {
+            return <Article key={item.id} {...item} />;
+          })}
+        </section>
+      </nav>
+    </main>
+  );
+};
 
 export default App;
